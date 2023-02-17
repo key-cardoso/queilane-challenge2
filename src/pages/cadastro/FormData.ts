3daf790691cb9b46405a05de4b0ea6101c33021b
key-cardoso <queilanecardoso7@gmail.com>
2023-02-17 01:36:26 -0300

    Alterando as variaveis do FormData

--- src/pages/cadastro/FormData.ts
+++ src/pages/cadastro/FormData.ts
@@ -2,6 +2,11 @@ export interface FormData {
     name: string;
     firstname: string;
     lastname: string;
+    birthdate: string,
+    country: string,
+    city: string,
+    email: string,
     message: string;
+    password: string;
   }
   
