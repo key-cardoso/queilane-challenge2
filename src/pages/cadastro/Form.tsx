f122f54d14a4e3eb8b4f757da589cbbd5865d1b3
key-cardoso <queilanecardoso7@gmail.com>
2023-02-18 23:30:26 -0300

    Atualizando as divs do formulario

--- src/pages/cadastro/Form.tsx
+++ src/pages/cadastro/Form.tsx
@@ -36,41 +36,43 @@ const Form: React.FC<FormProps> = ({ onSubmit }) => {
     <form onSubmit={handleSubmit} className={classes.form}>
       <h1 className={classes.titulo}>Wecolme,</h1>
       <h2 className={classes.sub}>Please, register to continue</h2>
-      <label>
-        first name:
-        <input type="text" name="firstname" placeholder='Your first name' value={data.firstname} onChange={handleChange} />
-      </label>
-      <label>
-        last name:
-        <input type="text" name="lastname" placeholder='Your last name' value={data.lastname} onChange={handleChange} />
-      </label>
-      <label>
-        birth date:
-        <input type="date" name="birthdate" placeholder='DD/MM/YYYY' value={data.birthdate} onChange={handleChange} />
-      </label>
-      <label>
-        country:
-        <input type="text" name="country" placeholder='Your Country' value={data.country} onChange={handleChange} />
-      </label>
-      <label>
-        city:
-        <input type="text" name="city" placeholder='Your City' value={data.city} onChange={handleChange} />
-      </label>
-      <label>
-        e-mail:
-        <input type=" e-mail" name=" e-mail" placeholder="A valid e-mail here" value={data.email} onChange={handleChange} />
-      </label>
-      <label>
-        password:
-        <input type="password" name="password" placeholder="Your password" value={data.email} onChange={handleChange} />
-      </label>
-      <label>
-        password:
-        <input type="password" name="password" placeholder="Comfirm your password" value={data.email} onChange={handleChange} />
-      </label>
-      
-      <button type="submit">Register Now</button>
-      <button type="submit">Login</button>
+      <div className= {classes.content}>
+        <div className={classes.campo}>
+          <label className={classes.label}>first name:</label>
+            <input type="text" name="firstname" placeholder='Your first name' value={data.firstname} onChange={handleChange} />
+        </div>
+        <div className={classes.campo}>
+          <label className={classes.label}>last name:</label>
+            <input type="text" name="lastname" placeholder='Your last name' value={data.lastname} onChange={handleChange} />
+        </div>
+        <div className={classes.campo}>
+          <label className={classes.label}>birth date:</label>
+            <input type="date" name="birthdate" placeholder='DD/MM/YYYY' value={data.birthdate} onChange={handleChange} />
+        </div>  
+        <div className={classes.campo}>
+          <label className={classes.label}>country:</label>
+            <input type="text" name="country" placeholder='Your Country' value={data.country} onChange={handleChange} />
+        </div> 
+        <div className={classes.campo}> 
+          <label className={classes.label}>city:</label>
+            <input type="text" name="city" placeholder='Your City' value={data.city} onChange={handleChange} />
+        </div>
+        <div className={classes.campo}>
+          <label className={classes.label}>e-mail:</label>
+            <input type=" e-mail" name=" e-mail" placeholder="A valid e-mail here" value={data.email} onChange={handleChange} />
+        </div>
+        <div className={classes.campo}>
+          <label className={classes.label}>password:</label>
+            <input type="password" name="password" placeholder="Your password" value={data.email} onChange={handleChange} />
+        </div>
+        <div className={classes.campo}>  
+          <label className={classes.label}>password:</label>
+            <input type="password" name="password" placeholder="Comfirm your password" value={data.email} onChange={handleChange} />
+        </div>  
+        <button className={classes.b1} type="submit">Register Now</button>
+        <button className={classes.b2} type="submit">Login</button>
+      </div>
+       
     </form>
     
   );
