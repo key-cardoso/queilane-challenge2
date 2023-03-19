80c38c6caf1136ffe097f5582e267e2270f9236e
key-cardoso <queilanecardoso7@gmail.com>
2023-03-19 13:52:04 -0300

    Inserindo rotas das páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Form from '../src/pages/cadastro/Form';
+import Login from '../src/pages/login/login';
+import Dashboard from './pages/dash/dash';
 import { FormData } from '../src/pages/cadastro/FormData';
-import imagem from './img/image1.png'
-import logo from './img/logo.png'
+import imagem from './img/image1.png';
+import logo from './img/logo.png';
+import { Switch, BrowserRouter, Route } from "react-router-dom";
 
 const App: React.FC = () => {
   const handleSubmit = (data: FormData) => {
@@ -10,18 +13,24 @@ const App: React.FC = () => {
   };
 
   return (
-    <div className='App'>
-      <div className='form-container'>
-        <Form  onSubmit={handleSubmit} />
-      </div>
-      
-      <div className='imagem-container'>
-        <img className='img' src={imagem} alt="Imagem" />
-        <a href="https://compass.uol/en/home/"><img className='logo' src={logo} alt="logo" /></a>
-      </div>
+    <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={() => (
+            <div className='App'>
+              <div className='form-container'>
+                <Form onSubmit={handleSubmit} />
+              </div>
+              <div className='imagem-container'>
+                <img className='img' src={imagem} alt="Imagem" />
+                <a href="https://compass.uol/en/home/" target="_blank" rel="noopener noreferrer"><img className='logo' src={logo} alt="logo" /></a>
+              </div>
+            </div>
+          )} />
 
-    </div>
-
+          <Route path="/login" component={Login} />
+          <Route path="/dashboard" component={Dashboard} />
+        </Switch>
+    </BrowserRouter>
   );
 };
 
